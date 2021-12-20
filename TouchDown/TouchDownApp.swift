f7203a8e2234ad4b62be99ff9e90813650d2680d
Dvaid Echianu <g.sol.group@gmail.com>
2021-12-20 10:09:39 +0100

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by david on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
